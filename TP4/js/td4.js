bcbde4083bdc33e9d0c8915a137589e0e3586e5f
Mateus <matlop1604@gmail.com>
2023-04-15 15:16:34 +0200

    ex2 td4

--- TP4/js/td4.js
+++ TP4/js/td4.js
@@ -54,7 +54,7 @@ personne.sports = {
 
 
 for (let prop in personne.sports) {
-    console.log(prop ,':', personne.sports[prop]);
+    console.log(prop, ':', personne.sports[prop]);
 }
 
 personne.sports.sport1 = {
@@ -73,14 +73,14 @@ personne.sports.sport3 = {
 }
 
 for (let prop in personne.sports) {
-    console.log(prop ,':', personne.sports[prop]);
+    console.log(prop, ':', personne.sports[prop]);
 }
 
-personne.qui = function() {
+personne.qui = function () {
     console.log(this.nom, this.prenom);
 }
 
-personne.quimaj = function() {
+personne.quimaj = function () {
     console.log(this.nom.toUpperCase(), this.prenom.toUpperCase());
 }
 
@@ -91,7 +91,7 @@ let div = document.createElement("div");
 
 let values = Object.values(personne);
 
-for (let i = 0; i<Object.values(personne).length; i++) {
+for (let i = 0; i < Object.values(personne).length; i++) {
     div.textContent += values[i];
 }
 
@@ -107,4 +107,87 @@ console.log(JSON.stringify(Object.values(personne)));
 
 // Exercice 2
 
+personne.langue = "Francais"
+
+personne.getLang = function () {
+    return this.langue
+}
+
+personne.setLang = function (newLangue) {
+    this.langue = newLangue
+}
+
+
+const obj = { counter: 0 };
+
+Object.defineProperty(obj, 'reset', {
+    get: function () {
+        this.counter = 0;
+    }
+});
+
+Object.defineProperty(obj, 'inc', {
+    get: function () {
+        this.counter++;
+    }
+});
+
+Object.defineProperty(obj, 'dec', {
+    get: function () {
+        this.counter--;
+    }
+});
+
+Object.defineProperty(obj, 'add', {
+    set: function (value) {
+        this.counter += value;
+    }
+});
+
+Object.defineProperty(obj, 'subs', {
+    set: function (value) {
+        this.counter -= value;
+    }
+});
+
+// 2.2
+
+function Personne(nom, prenom, age, couleurYeux) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.age = age;
+    this.couleurYeux = couleurYeux;
+
+    this.name = function () {
+        console.log(`Je m'appelle ${this.prenom} ${this.nom}`);
+    }
+
+    this.changeName = function (newNom, newPrenom) {
+        this.nom = newNom;
+        this.prenom = newPrenom;
+    }
+}
+
+const personne1 = new Personne('Dupont', 'Jean', 30, 'marron');
+personne1.changeName('Durand', 'Pierre');
+personne1.name(); 
+
+
+let x1 = "Hello";
+console.log(x1.length); 
+
+let x2 = 3;
+console.log(x2.toString()); 
+
+let x3 = true;
+console.log(x3.valueOf()); 
+
+let x4 = null;
+console.log(x4 === null); 
+
+console.log(Math.PI); 
+
+console.log(Math.pow(2, 3)); 
+
+console.log(Math.sqrt(25));
 
